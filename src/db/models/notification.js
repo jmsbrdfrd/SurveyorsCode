73b62bca057c1ff54095c568933d8bc3b40c8b5e
jmsbrdfrd <58705345+jmsbrdfrd@users.noreply.github.com>
2021-01-20 20:45:44 +0000

    Added notification model

---
+++ src/db/models/notification.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+
+const notificationSchema = new mongoose.Schema({
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        trim: true
+    },
+    from: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        trim: true
+    },
+    message: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    date: {
+        type: Date,
+        required: true,
+        trim: true
+    },
+    link: {
+        type: String,
+        required: true,
+        trim: true
+    }
+})
+
+const Notification = mongoose.model('Notification', notificationSchema)
+module.exports = Notification
